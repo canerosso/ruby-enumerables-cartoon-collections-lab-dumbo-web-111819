944a5c4fb228b43dab83d627a8c77a00c97e1949
Cane Rosso <astor@usa.com>
2019-11-11 04:25:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,4 @@
-dwarves = %w[
-  Doc
-  Dopey
-  Bashful
-  Grumpy
-  Sneezy
-  Sleepy
-  Happy
-]
+
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   dwarves.each_with_index do |dwarf, index| #utilizes "each_with_index" method
@@ -24,8 +16,9 @@ def long_planeteer_calls# code an argument here
   # Your code here
 end
 
-cheese = %[]
-def find_the_cheese()# code an argument here
+def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  
 end
